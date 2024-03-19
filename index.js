24b4dab44081b9de51dab9676a2a65ab8d17a212
Victor kamau <Victor Wangari>
2024-03-19 11:45:58 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+let random = 0
+random ++
+console.log(random)
+const num3 = 24
+const num4 =10
+let mod = num3 % num4
+let max = 20
+Math.max(5, 10, 15, 20)
 // Write your code here
